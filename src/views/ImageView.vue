e5a5e490e433a5ee87d6743a046c55048b8907ae
david-pilny <93579404+david-pilny@users.noreply.github.com>
2023-11-26 14:22:10 +0100

    feat(replicate): ask questions about an image

--- src/views/ImageView.vue
+++ src/views/ImageView.vue
@@ -1,3 +1,28 @@
-<script setup></script>
+<script setup>
+import ChatWindowImage from '../components/ChatWindowImage.vue'
+import { useImageChatStore } from '../stores/imageChat.cjs'
 
-<template></template>
+const imageChatStore = useImageChatStore()
+</script>
+
+<template>
+  <article class="grid grid-cols-1 lg:grid-cols-2 gap-x-8">
+    <div class="mt-4">
+      <h3>Add image and ask questions about it</h3>
+
+      <section class="my-4">
+        <input v-model="imageChatStore.imageURL" />
+      </section>
+
+      <img :src="imageChatStore.imageURL" class="w-80 mt-10" />
+    </div>
+
+    <div class="min-h-80 flex flex-col justify-between">
+      <ChatWindowImage />
+
+      <div class="flex justify-end">
+        <button @click="imageChatStore.clearChat()" class="button button-secondary">Clear</button>
+      </div>
+    </div>
+  </article>
+</template>
